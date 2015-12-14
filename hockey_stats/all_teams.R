84c7fc9fece1f96e2d9bd5d748a4e28e4e17559d
Sean <brookforestscitech@gmail.com>
2015-12-13 21:40:26 -0700

    Adding basic all teams data (NHL) and size processing script for all teams

---
+++ hockey_stats/all_teams.R
@@ -0,0 +1,7 @@
+# This R script file contains pertinent information about all teams to aid 
+# further processing
+
+teams <- c("ANA", "ARI", "BOS", "BUF", "CGY", "CAR", "CHI", "COL", "CBJ", 
+           "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NSH", "NJD", 
+           "NYI", "NYR", "OTT", "PHI", "PIT", "SJS", "STL", "TBL", "TOR", 
+           "VAN", "WSH", "WPG")
