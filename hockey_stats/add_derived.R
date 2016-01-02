66196dd38d0984a7ff9c49e64321076f9f92c395
Sean <brookforestscitech@gmail.com>
2016-01-01 22:18:57 -0700

    Yet more data munging goodness

--- hockey_stats/add_derived.R
+++ hockey_stats/add_derived.R
@@ -16,17 +16,23 @@ add.derived <- function(clean.data) {
   # Add total games to date for various averages
   clean.data <- cbind(clean.data, as.integer(rownames(clean.data)))
   
+  # Add win/loss deviations (depends on variance_calculations.R)
+  # This doesn't really make much sense unless I use it in terms of 
+  # total wins and losses.
+  clean.data <- cbind(clean.data, get.sd(clean.data$W))
+  clean.data <- cbind(clean.data, get.sd(clean.data$L))
+  
   # And give them names
   colnames(clean.data)[15] <- "GF.total"
   colnames(clean.data)[16] <- "GA.total"
   colnames(clean.data)[17] <- "Games.total"
+  colnames(clean.data)[18] <- "W.dev"
+  colnames(clean.data)[19] <- "L.dev"
   
   clean.data %>% rowwise() %>%
-    mutate(W.dev = sd(W),
-           L.dev = sd(L),
-           GF.avg = GF.total / Games.total,
+    mutate(GF.avg = GF.total / Games.total,
            GA.avg = GA.total / Games.total,
-           GFGA.ratio = GF.total / GA.total,
-           W.pct = W / Games.total,
-           WL.ratio = W / L)
+           GFGA.ratio = GF.total / GA.total)
+           #W.pct = W / Games.total, # Need total wins and losses to make this work
+           #WL.ratio = W / L) # Same as above, for total.
 }
