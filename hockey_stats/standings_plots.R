82d65ba44c35664433908c5a55d17c768103411d
progdrum <brookforestscitech@gmail.com>
2016-01-22 16:32:55 -0700

    Bringing plots and plot code to see the light of day

--- hockey_stats/standings_plots.R
+++ hockey_stats/standings_plots.R
@@ -8,7 +8,7 @@ wins <- c(atlantic.avg.W, metropolitan.avg.W, central.avg.W, pacific.avg.W)
 win.avgs <- cbind.data.frame(division.names, wins)
 colnames(win.avgs) <- c("Divisions", "Wins")
 
-p <- ggplot(win.avgs, aes(x = Divisions, y = Wins, fill = Divisions)) + 
-  geom_bar(stat = "identity") + ggtitle("Average Wins By Division (NHL)")
-
-plot(p)
+p1 <- ggplot(win.avgs, aes(x = Divisions, y = Wins, fill = Divisions)) + 
+  geom_bar(stat = "identity") + 
+  geom_text(aes(label = round(Wins, 2))) + 
+  ggtitle("Average Wins By Division")
