92dfb5f18859fff34d4f1b912f3fb7222fb9b503
progdrum <brookforestscitech@gmail.com>
2016-01-27 18:53:07 -0700

    Added a couple more graphs

--- hockey_stats/standings_plots.R
+++ hockey_stats/standings_plots.R
@@ -8,7 +8,8 @@ division.names <- c("Atlantic", "Metropolitan", "Central", "Pacific")
 wins <- c(atlantic.avg.W, metropolitan.avg.W, central.avg.W, pacific.avg.W)
 losses <- c(atlantic.avg.L, metropolitan.avg.L, central.avg.L, pacific.avg.L)
 ols <- c(atlantic.avg.OL, metropolitan.avg.OL, central.avg.OL, pacific.avg.OL)
-
+pts <- c(atlantic.avg.pts, metropolitan.avg.pts, central.avg.pts, pacific.avg.pts)
+ptsp <- c(atlantic.avg.ptsp, metropolitan.avg.ptsp, central.avg.ptsp, pacific.avg.ptsp)
 win.avgs <- cbind.data.frame(division.names, wins)
 colnames(win.avgs) <- c("Divisions", "Wins")
 
@@ -21,6 +22,12 @@ colnames(ol.avgs) <- c("Divisions", "OT.Losses")
 stack.avgs <- cbind.data.frame(division.names, wins, losses, ols)
 colnames(stack.avgs) <- c("Divisions", "Wins", "Losses", "OT.Losses")
 stack.avgs2 <- melt(stack.avgs, id.vars = "Divisions")
+
+pts.avgs <- cbind.data.frame(division.names, pts)
+colnames(pts.avgs) <- c("Divisions", "Points")
+
+ptsp.avgs <- cbind.data.frame(division.names, ptsp)
+colnames(ptsp.avgs) <- c("Divisions", "Points.Pct")
 
 p1 <- ggplot(win.avgs, aes(x = Divisions, y = Wins, fill = Divisions)) + 
   geom_bar(stat = "identity") + 
@@ -50,4 +57,14 @@ p5 <- ggplot(stack.avgs2, aes(x = Divisions, y = value, fill = variable)) +
   geom_bar(stat = "identity", position = "fill") + 
   ylab("Normalized Value") + 
   ggtitle("Summary of Wins, Losses, and OT Losses (Average) By Division") + 
+  guides(fill = FALSE)
+
+p6 <- ggplot(pts.avgs, aes(x = Divisions, y = Points, fill = Divisions)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle("Average Points By Division") + 
+  guides(fill = FALSE)
+
+p7 <- ggplot(ptsp.avgs, aes(x = Divisions, y = Points.Pct, fill = Divisions)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle("Average Points % By Division") + 
   guides(fill = FALSE)
